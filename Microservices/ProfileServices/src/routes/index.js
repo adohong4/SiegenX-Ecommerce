a5742554c226a6ac782d6e61bdb0b3595c5e7a38
adohong4 <adohong4@gmail.com>
2025-02-13 16:03:47 +0700

    route cart

--- Microservices/ProfileServices/src/routes/index.js
+++ Microservices/ProfileServices/src/routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/v1/api', require('./profile'));
+router.use('/v1/api', require('./cart'));
 
 
 module.exports = router;

c2bfd8658a329261daea40e2dbf022f1b39d7053
adohong4 <adohong4@gmail.com>
2025-02-13 09:43:39 +0700

    Cart succesfully

--- Microservices/ProfileServices/src/controller/cart.controller.js
+++ Microservices/ProfileServices/src/controller/cart.controller.js
@@ -1,30 +1,63 @@
 'use strict'
 
 const cartService = require('../services/cart.service')
+const { OK } = require('../utils/statusCodes')
 
 class CartController {
-    addToCart = async (req, res) => {
+    addToCart = async (req, res, next) => {
         try {
             const { userId } = req.user._id
             const { itemId } = req.body
-            const cart = await cartService.addToCart(userId, itemId)
-            res.status(200).json(cart)
+            const result = await cartService.addToCart(userId, itemId)
+            new CREATED({
+                message: 'Thêm vào giỏ hàng thành công',
+                metadata: result.cart
+            }).send(res)
         } catch (error) {
-            res.status(500).json({ message: error.message })
+            next(error)
         }
     }
 
-    getCart = async (req, res) => {
+    addQuantityToCart = async (req, res, next) => {
+        try {
+            const { userId } = req.user._id
+            const { itemId, quantity } = req.body
+            const result = await cartService.addQuantityToCart(userId, itemId, quantity)
+            new CREATED({
+                message: 'Thêm vào giỏ hàng thành công',
+                metadata: result.cart
+            }).send(res)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    getCart = async (req, res, next) => {
         try {
             const { userId } = req.user._id
             const cart = await cartService.getCart(userId)
-            res.status(200).json(cart)
+            new OK({
+                message: 'Lấy giỏ hàng',
+                metadata: cart.cart
+            }).send(res)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    removeFromCart = async (req, res, next) => {
+        try {
+            const { userId } = req.user._id
+            const { itemId } = req.body
+            const cart = await cartService.removeFromCart(userId, itemId)
+            new OK({
+                message: 'Xoa thanh cong',
+                metadata: cart.cart
+            })
         } catch (error) {
             res.status(500).json({ message: error.message })
         }
     }
-
-
 }
 
 module.exports = new CartController();
--- Microservices/ProfileServices/src/services/cart.service.js
+++ Microservices/ProfileServices/src/services/cart.service.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const userCart = require('../models/profile.model')
+const { BadRequestError, ConflictRequestError, AuthFailureError, ForbiddenError } = require("../core/error.response")
 
 class UserCartService {
 
@@ -28,6 +29,46 @@ class UserCartService {
 
             return {
                 cart: user.cartData
+            }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    static addQuantityToCart = async (userId, itemId, quantity) => {
+        try {
+            let user = await userCart.findById(userId)
+            let cartData = await user.cartData;
+
+            if (quantity < 0) {
+                throw new BadRequestError('Quantity must be greater than 0')
+            }
+
+            if (!cartData[itemId]) {
+                cartData[itemId] = quantity;
+            } else {
+                cartData[itemId] += quantity;
+            }
+
+            const updatedCart = await userCart.findByIdAndUpdate(userId, { cartData })
+            return {
+                cart: updatedCart.cartData
+            }
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    static removeFromCart = async (userId, itemId) => {
+        try {
+            let user = await userCart.findById(userId)
+            let cartData = await user.cartData;
+            if (cartData[itemId] > 0) {
+                cartData[itemId] -= 1;
+            }
+            const updatedCart = await userCart.findByIdAndUpdate(userId, { cartData })
+            return {
+                cart: updatedCart.cartData
             }
         } catch (error) {
             throw new Error(error)

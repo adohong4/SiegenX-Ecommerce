c4d70aaec1bc858259c1259c8ba8d3abe639fedc
adohong4 <adohong4@gmail.com>
2025-03-14 01:14:16 +0700

    add redis at SupplierService

---
+++ Microservices/SupplierServices/src/services/redis.service.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { getRedis } = require('../config/init.redis')
+const supplierModel = require('../models/supplier.model')
+const { RedisErrorResponse } = require('../core/error.response')
+
+class RedisService {
+
+}
+
+module.exports = RedisService;
